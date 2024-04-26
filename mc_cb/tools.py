19ecb7bb340b9d01a0b8be9e39306a80c3adff80
slexce <2767145231@qq.com>
2024-04-27 00:12:34 +0800

    update 在tools.cb_image中暂时实现了setblock平铺方法，添加了tqdm进度条

--- mc_cb/tools.py
+++ mc_cb/tools.py
@@ -1,17 +1,23 @@
+"""
+通过function执行更改游戏的类与方法
+"""
+
 from math import sqrt
 import PIL.JpegImagePlugin
 from functools import cache
 from PIL import Image
 import numpy as np
-from typing import Callable,overload,Literal
+from tqdm import tqdm
 
-from .game import position
+from .game import position_list
 from ._block_infor import color_block_62
+from .commands import setblock
 
 class cb_image:
     '''对图像进行处理'''
     def __init__(self,file:str | PIL.JpegImagePlugin.JpegImageFile,width:int=None,height:int=None,quality:int=5) -> None:
         self.file=file
+        self.image_quality=quality
         if isinstance(self.file,str):
             self.png=Image.open(file)
         else:
@@ -27,42 +33,81 @@ class cb_image:
             width=int(png.size[0]*(height/png.size[1]))
             png=png.resize((width,height),quality)
         self.png=png
+        self.width,self.height=png.size
+        self.png_data=png.getdata()
+        self.RGB=np.asarray(self.png_data).reshape(self.height,self.width,3)
     
-    @property
-    def png_data(self):
-        return self.png.getdata()
-    
-    @property
-    def width(self):
-        return self.png.size[0]
-    
-    @property
-    def height(self):
-        return self.png.size[1]
-    
-    @property
-    def RGB(self):
-        return np.asarray(self.png_data).reshape(self.height,self.width,3)
-    
-    image_direction=Literal["x+","x-","y+","y-","z+","z-"]
-    image_setblock_mode=Literal["cut","not_cut","resize",""]
-    
-    def setblock(self,point_1: str | list[int]="~~~",point_2: str | list[int]="~~~",mode:image_setblock_mode="no_cut",direction:image_direction="y+",color_dict:dict=color_block_62,map_mode:bool=False):
+    def setblock(self,pos_1: str | list[int],pos_2: str | list[int]=None,need_cut:bool=False,color_dict:dict=color_block_62,map_mode:bool=False):
         '''在世界中放置像素画
-        - point_1,point_2 坐标
-        - mode 模式，
-            - cut 超出坐标范围剪切
-            - not_cut 默认模式，不剪切超出范围的区域
-            - resize 缩放，缩放图片大小符合区域大小
-        - direction 图像正方向
-        - color_dict 方块名称与方块颜色RGB值的对照字典
+        - pos_1 起始坐标
+        - pos_2 终点坐标
+        - need_cut 是否裁切超出范围的图像部分
+        - color_dict 方块名称与方块颜色RGB值的对照字典 {"block_name":(R,B,G)} r,g,b：int
         - map_mode 是否启用在手持地图上显示更加真实的模式'''
-        point_1 = str(position(point_1))
-        point_2 = str(position(point_2))
-        if mode == "cut":
-            self.png = self.png.resize(())
+        tmp_dis = None
+        pos_1 = position_list.parse_pos(pos_1)
+        pos_2 = position_list.parse_pos(pos_2)
+        poss = position_list(pos_1,pos_2)
         
+        if not poss.isSamePlane:
+            raise ValueError(f"{pos_1},{pos_2}坐标不在一个平面")
+        if poss.vector()[1] != 0:
+            height = poss.len_y
+            width = poss.len_z if poss.len_x == 1 else poss.len_x
+        else:
+            height = poss.len_x
+            width = poss.len_z
+        
+        png = cb_image(self.png)
+        if need_cut:
+            #裁切
+            png.RGB = png.RGB[:height-1,:width-1]
+        else:
+            #不剪切
+            height = png.height
+            width = png.width
+            x,y,z = 0,0,0
+            
+            if tmp := poss.vector_y()[1]:
+                y = tmp/abs(tmp)*(height-1)
+                if tmp := poss.vector_x()[0]:
+                    x = tmp/abs(tmp)*(width-1)
+                
+            if tmp := poss.vector_z()[2]:
+                z = tmp/abs(tmp)*(height-1)
+                if tmp := poss.vector_x()[0]:
+                    x = tmp/abs(tmp)*(width-1)
+            
+            pos_2 = list(map(int,[x + pos_1[0],y + pos_1[1],z + pos_1[2]]))
+            poss = position_list(pos_1,pos_2)
+
+            if poss.vector()[1]:
+                positions = iter(poss.positions[-1::-1])
+            else:
+                positions = iter([j for i in poss.lines for j in i])
+        
+        if map_mode:
+            if poss.vector()[1] == 0:
+                raise ValueError(f"地图模式需平铺像素画，而不是垂直于水平面")
+            ...
+        else:
+            for i in tqdm(range(height)):
+                for j in tqdm(range(width),leave=False):
+                    rgb_1 = tuple(png.RGB[i][j])
+                    for name,rgb in color_dict.items():
+                        co_dis = self.color_distance(rgb_1,rgb)
+                        if tmp_dis == None:
+                            tmp_dis = co_dis
+                            tmp_name = name
+                        if tmp_dis  > co_dis:
+                            tmp_dis = co_dis
+                            tmp_name = name
+                    tmp_dis = None
+                    pos = next(positions)
+                    setblock(pos,tmp_name)
+
     @staticmethod
+    @cache
     def _color_distance_formula(R_mean,R,G,B):
         return sqrt((2+R_mean/156)*(R**2)+4*(G**2)+(2+(256-R_mean)/256)*(B**2))
 
